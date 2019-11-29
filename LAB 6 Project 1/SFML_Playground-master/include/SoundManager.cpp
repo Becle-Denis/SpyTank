5d948e19e154686eb1e5574ad15bae52e64c5bec
Denis <c00254311@itcarlow.ie>
2019-11-29 18:56:33 +0000

    Cleaning soundManager

--- LAB 6 Project 1/SFML_Playground-master/include/SoundManager.cpp
+++ LAB 6 Project 1/SFML_Playground-master/include/SoundManager.cpp
@@ -8,7 +8,6 @@ SoundManager::SoundManager(std::string filePath)
 		std::string s("Error loading music "+ filePath);
 		throw std::exception(s.c_str());
 	}
-	
 	if (!m_menuMusic.openFromFile("./resources/sounds/music/MenuMusic.ogg"))
 	{
 		std::string s("Error loading music ./resources/sounds/music/MenuMusic.ogg");
@@ -20,44 +19,53 @@ SoundManager::SoundManager(std::string filePath)
 		std::string s("Error loading music ./resources/sounds/fx/fire1.wav");
 		throw std::exception(s.c_str());
 	}
-	m_fireSound.setBuffer(m_fireBuffer);
-
+	
 	if (!m_targetImpactBuffer.loadFromFile("./resources/sounds/fx/targetImpact2.wav"))
 	{
 		std::string s("Error loading music ./resources/sounds/fx/targetImpact2.wav");
 		throw std::exception(s.c_str());
 	}
-	m_targetImpactSound.setBuffer(m_targetImpactBuffer);
-
+	
 	if (!m_targetStartBuffer.loadFromFile("./resources/sounds/fx/TargetSonarStart.wav"))
 	{
 		std::string s("Error loading music ./resources/sounds/fx/TargetSonarStart.wav");
 		throw std::exception(s.c_str());
 	}
-	m_targetStartSound.setBuffer(m_targetStartBuffer);
-
+	
 	if (!m_wallImpactBuffer.loadFromFile("./resources/sounds/fx/WallImpact1.wav"))
 	{
 		std::string s("Error loading music ./resources/sounds/fx/WallImpact1.wav");
 		throw std::exception(s.c_str());
 	}
+	
+	//setting buffer
+	m_fireSound.setBuffer(m_fireBuffer);
+	m_targetImpactSound.setBuffer(m_targetImpactBuffer);
+	m_targetStartSound.setBuffer(m_targetStartBuffer);
 	m_wallImpactSound.setBuffer(m_wallImpactBuffer);
 
-	//mixing 
+	//setting relative 
 	m_levelMusic.setRelativeToListener(false);
-	m_levelMusic.setVolume(70);
 	m_menuMusic.setRelativeToListener(false);
+	
+	m_targetImpactSound.setRelativeToListener(true);
+	m_targetStartSound.setRelativeToListener(true);
+	m_wallImpactSound.setRelativeToListener(true);
+
+
+	//setting distances 
+	m_targetImpactSound.setMinDistance(100);
+	m_targetStartSound.setMinDistance(100);
+	m_wallImpactSound.setMinDistance(100);
+
+	//mixing 
+	m_levelMusic.setVolume(70);
 	m_menuMusic.setVolume(80);
+
 	m_fireSound.setVolume(100);
 	m_targetImpactSound.setVolume(100);
-	m_targetImpactSound.setRelativeToListener(true);
-	m_targetImpactSound.setMinDistance(100);
 	m_targetStartSound.setVolume(100);
-	m_targetStartSound.setRelativeToListener(true);
-	m_targetStartSound.setMinDistance(100);
 	m_wallImpactSound.setVolume(100);
-	m_wallImpactSound.setRelativeToListener(true);
-	m_wallImpactSound.setMinDistance(100);
 
 }
 
